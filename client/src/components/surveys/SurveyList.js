22cde9d0b6669fbd778881525542c9afb83cbb5d
EmanueleDB <leleoioioi@gmail.com>
2020-02-26 16:26:38 +0100

    bugfixing

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -10,7 +10,7 @@ class SurveyList extends Component {
   renderSurveys() {
     return this.props.surveys.reverse().map(survey => {
       return (
-        <div className="card blue-grey darken-1" key={survey._id}>
+        <div style={{color: 'white'}} className="card blue-grey darken-1" key={survey._id}>
           <div className="card-content">
             <span className="card-title">{survey.title}</span>
             <p>{survey.body}</p>
@@ -19,6 +19,7 @@ class SurveyList extends Component {
             </p>
           </div>
           <div className="card-action">
+            User's Feedback: <br/>
             <a>Yes: {survey.yes}</a>
             <a>No: {survey.no}</a>
           </div>
